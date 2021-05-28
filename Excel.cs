5045a849d6134861bfac190d3366e1082fa05c8d
Usaty Roman <boss.usaty@inbox.ru>
2021-05-28 14:55:23 +0300

    Добавьте файлы проекта.

---
+++ Excel.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ClosedXML.Excel;
+using System.IO;
+
+namespace ExcelHelper
+{
+    public class Excel
+    {
+        public List<List<string>> Table = new List<List<string>>();
+
+
+        // copywrite with Github (because with my VS is doesn`t work). Creator ukushu. Original: https://github.com/ukushu/DataExporter
+        // 
+        public void FileOpen(string path)
+        {
+
+            var workbook = new XLWorkbook(path);
+            var ws1 = workbook.Worksheet(1);
+
+            foreach (var xlRow in ws1.RangeUsed().Rows())
+            {
+                Table.Add(new List<string>());
+
+                foreach (var xlCell in xlRow.Cells())
+                {
+                    var formula = xlCell.FormulaA1;
+                    var value = xlCell.Value.ToString();
+
+                    string targetCellValue = (formula.Length == 0) ? value : "=" + formula;
+
+                    Table[Table.Count - 1].Add(targetCellValue);
+                }
+            }
+        }
+
+        public void FileSave(string path)
+        {
+            CreateDirIfNotExist(path, true);
+
+            using (XLWorkbook wb = new XLWorkbook())
+            {
+                var workSheet = wb.Worksheets.Add("Sample Sheet");
+
+                for (int row = 0; row < Table.Count; row++)
+                {
+                    for (int col = 0; col < Table[row].Count; col++)
+                    {
+                        var cellAdress = GetExcelPos(row, col);
+
+                        if (Table[row][col].StartsWith("="))
+                        {
+                            workSheet.Cell(cellAdress).FormulaA1 = Table[row][col];
+                        }
+                        else
+                        {
+                            workSheet.Cell(cellAdress).Value = Table[row][col];
+                        }
+                    }
+                }
+
+                wb.SaveAs(path);
+            }
+        }
+
+        public void AddRow(params string[] cells)
+        {
+            Table.Add(cells.ToList());
+        }
+
+        public static string GetExcelPos(int row, int cell)
+        {
+            char[] alph = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+
+            int count = cell / 26;
+            string alphResult;
+
+            alphResult = count > 0 ? alph[count] + alph[count % 26].ToString() : alph[cell].ToString();
+
+            return alphResult + (row + 1);
+        }
+
+        private void CreateDirIfNotExist(string dirPath, bool removeFilename = false)
+        {
+            if (removeFilename)
+            {
+                dirPath = Directory.GetParent(dirPath).FullName;
+            }
+
+            if (!Directory.Exists(dirPath))
+            {
+                _ = Directory.CreateDirectory(dirPath);
+            }
+        }
+    }
+}
